309c3ab912ea99ca300b9647b03540cecc83432a
Atul Srivastava <atul.srivastava@bloxroute.com>
2022-12-07 13:35:28 -0500

    test linter

--- utils/error.ts
+++ utils/error.ts
@@ -2,7 +2,7 @@ export interface RpcError {
     code: number
     message: string
 }
-
+//testfail
 export function isRpcError(obj: any): boolean {
     return typeof obj.code == "number" && typeof obj.message == "string"
 }
