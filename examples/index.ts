1beab481b7bec808d9dde3f2adef3bd30c1b1186
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2023-02-17 17:23:44 -0500

    increase timeout

--- examples/index.ts
+++ examples/index.ts
@@ -61,8 +61,8 @@ const testOrder: PostOrderRequest = {
 }
 
 const transactionWaitTimeS = 60
-const httpTimeout = 10_000
-const httpLongTimeout = 20_000
+const httpTimeout = 30_000
+const httpLongTimeout = 60_000
 
 function delay(milliseconds: number) {
     return new Promise((resolve) => setTimeout(resolve, milliseconds))
