8d05b5ba94acbb725c8618f0891b9be244505c37
Kevin Chen <kevin@bloxroute.com>
2023-06-12 12:39:58 -0500

    TRAD-684 lt consts

--- bxsolana/index.browser.ts
+++ bxsolana/index.browser.ts
@@ -1,28 +1,37 @@
-export { BaseProvider } from "./provider/base"
-export { HttpProvider } from "./provider/http"
-export { WsProvider } from "./provider/ws"
+export { BaseProvider } from "./provider/base";
+export { HttpProvider } from "./provider/http";
+export { WsProvider } from "./provider/ws";
 export {
     LOCAL_API_GRPC_HOST,
     LOCAL_API_GRPC_PORT,
     LOCAL_API_HTTP,
     LOCAL_API_WS,
-    MAINNET_API_GRPC_HOST,
     MAINNET_API_GRPC_PORT,
-    MAINNET_API_HTTP,
-    MAINNET_API_WS,
+    MAINNET_API_VIRGINIA_GRPC,
+    MAINNET_API_VIRGINIA_HTTP,
+    MAINNET_API_VIRGINIA_WS,
+    MAINNET_API_NY_GRPC,
+    MAINNET_API_NY_HTTP,
+    MAINNET_API_NY_WS,
+    MAINNET_API_UK_GRPC,
+    MAINNET_API_UK_HTTP,
+    MAINNET_API_UK_WS,
+    MAINNET_API_FRANKFURT_GRPC,
+    MAINNET_API_FRANKFURT_HTTP,
+    MAINNET_API_FRANKFURT_WS,
     TESTNET_API_GRPC_HOST,
     TESTNET_API_GRPC_PORT,
     TESTNET_API_HTTP,
-    TESTNET_API_WS,
-} from "./utils/constants"
+    TESTNET_API_WS
+} from "./utils/constants";
 
 export {
     addMemo,
     addMemoToSerializedTxn,
     buildFullySignedTxn,
-    createTraderAPIMemoInstruction,
-} from "./utils/memo"
+    createTraderAPIMemoInstruction
+} from "./utils/memo";
 
-export { signTx, signTxMessage } from "./utils/transaction"
+export { signTx, signTxMessage } from "./utils/transaction";
 
-export * from "./proto/messages/api"
+export * from "./proto/messages/api";
