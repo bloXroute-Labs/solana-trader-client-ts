879aeeae2c72ad312ffc7777f207640e25955443
atulsriv <atul.srivastava@bloxroute.com>
2024-02-12 16:59:53 -0500

    resolve PR comment

--- bxsolana/utils/bundletip.ts
+++ bxsolana/utils/bundletip.ts
@@ -7,8 +7,9 @@ import {
     VersionedTransaction,
     SystemProgram
 } from "@solana/web3.js"
-
-const TRADER_API_TIP_WALLET = "AFT8VayE7qr8MoQsW3wHsDS83HhEvhGWdbNSHRKeUDfQ"
+// check documentation for latest tip wallet, and how to send tip transactions
+// https://docs.bloxroute.com/solana/trader-api-v2/front-running-protection-and-transaction-bundle
+const TRADER_API_TIP_WALLET = "HWEoBxYs7ssKuudEjzjmpfJVX7Dvi7wescFsVx2L5yoY"
 
 // createTraderAPIMemoInstruction generates a transaction instruction that places a memo in the transaction log
 // Having a memo instruction with signals Trader-API usage is required
