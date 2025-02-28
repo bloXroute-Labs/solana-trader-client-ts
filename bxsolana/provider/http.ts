250e88fec530bc5a1cbc45ca581821ca47cbe78f
Jose <jose.bravo@bloxroute.com>
2025-02-28 10:25:05 -0700

    Proto bump (#141)

--- bxsolana/provider/http.ts
+++ bxsolana/provider/http.ts
@@ -177,10 +177,7 @@ export class HttpProvider extends BaseProvider {
     getJupiterQuotes(
         request: GetJupiterQuotesRequest
     ): RpcReturnType<Promise<GetJupiterQuotesResponse>, []> {
-        let path = `${this.baseUrlV2}/jupiter/quotes?inToken=${request.inToken}&outToken=${request.outToken}&inAmount=${request.inAmount}&slippage=${request.slippage}`
-        if (request.fastMode) {
-            path += `&fastMode=${request.fastMode}`
-        }
+        const path = `${this.baseUrlV2}/jupiter/quotes?inToken=${request.inToken}&outToken=${request.outToken}&inAmount=${request.inAmount}&slippage=${request.slippage}`
         return this.get<GetJupiterQuotesResponse>(path)
     }
 
